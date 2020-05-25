0e2f86df7eebec2ec1d68101adf94fce4035432a
mdtaylor76 <62409954+mdtaylor76@users.noreply.github.com>
2020-05-24 19:06:53 -0700

    Framework

---
+++ Project1/src/main/java/com/csumb/cst363/project1/Project1.java
@@ -0,0 +1,13 @@
+package com.csumb.cst363.project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1.class, args);
+	}
+
+}

68a93fe5daae7b2454285123f1450b5955661e6a
Ryan Barrett <ryan.barrett@spookyhouse.org>
2020-05-26 21:38:58 -0700

    resolve conflicts in Prescription

--- Project1/src/main/java/com/csumb/cst363/project1/Prescription.java
+++ Project1/src/main/java/com/csumb/cst363/project1/Prescription.java
@@ -2,21 +2,77 @@ package com.csumb.cst363.project1;
 
 import java.sql.Date;
 
-public class Prescription {
+public class Prescription
+{
     int doctorID;
     int patientID;
-    String drugName = "w";
-    int quantity;
+    String drugName;
+    int drugID;
+    int dosage;
+    int pharmacyID;
     int refills;
-    Date date;
-    //int prescriptionID;
+    String date;
+    Date sqlDate;
 
-    public int getPrescriptionID() {
+    public Date getSqlDate() {
+        return sqlDate;
+    }
+
+    public String getDrugName() {
+        return drugName;
+    }
+
+    public String getDate()
+    {
+        return date;
+    }
+
+    public void setDate(String date)
+    {
+        this.date = date;
+    }
+
+    public int getDosage()
+    {
+        return dosage;
+    }
+
+    public void setDosage(int dosage)
+    {
+        this.dosage = dosage;
+    }
+
+    public int getPharmacyID()
+    {
+        return pharmacyID;
+    }
+
+    public void setPharmacyID(int pharmacyID)
+    {
+        this.pharmacyID = pharmacyID;
+    }
+
+    int quantity;
+
+    public int getDrugID()
+    {
+        return drugID;
+    }
+
+    public void setDrugID(int drugID)
+    {
+        this.drugID = drugID;
+    }
+
+
+    public int getPrescriptionID()
+    {
         //return prescriptionID;
         return 0;
     }
 
-    public void setPrescriptionID(int prescriptionID) {
+    public void setPrescriptionID(int prescriptionID)
+    {
         // this.prescriptionID = prescriptionID;
     }
 
@@ -24,67 +80,70 @@ public class Prescription {
 
     }
 
+    public Prescription(int doctorID, int patientID,
+                        int quantity, int refills) {
+        this.doctorID = doctorID;
+        this.patientID = patientID;
+        this.quantity = quantity;
+        this.refills = refills;
+    }
+
     public Prescription(int doctorID, int patientID, String drugName,
                         int quantity, int refills, Date date) {
         this.doctorID = doctorID;
         this.patientID = patientID;
-        this.drugName = drugName;
         this.quantity = quantity;
         this.refills = refills;
-        this.date = date;
+        this.sqlDate = date;
+        this.drugName = drugName;
     }
 
-    public Prescription(int doctorID, int patientID, String drugName,
+    public Prescription(int doctorID, int patientID,
                         int quantity, int refills, int prescriptionID) {
         this.doctorID = doctorID;
         this.patientID = patientID;
-        this.drugName = drugName;
         this.quantity = quantity;
         this.refills = refills;
         //this.prescriptionID = prescriptionID;
     }
 
-    public int getDoctorID() {
+    public int getDoctorID()
+    {
         return doctorID;
     }
 
-    public void setDoctorID(int doctorSSN) {
+    public void setDoctorID(int doctorID)
+    {
         this.doctorID = doctorID;
     }
 
-    public int getPatientID() {
+    public int getPatientID()
+    {
         return patientID;
     }
 
-    public void setPatientID(int patientID) {
+    public void setPatientID(int patientID)
+    {
         this.patientID = patientID;
     }
 
-    public String getDrugName() {
-        return drugName;
-    }
-
-    public void setDrugName(String drugName) {
-        this.drugName = drugName;
-    }
-
-    public int getQuantity() {
+    public int getQuantity()
+    {
         return quantity;
     }
 
-    public void setQuantity(int quantity) {
+    public void setQuantity(int quantity)
+    {
         this.quantity = quantity;
     }
 
-    public int getRefills() {
+    public int getRefills()
+    {
         return refills;
     }
 
-    public void setRefills(int refills) {
+    public void setRefills(int refills)
+    {
         this.refills = refills;
-    }
-
-    public Date getDate() {
-        return date;
     }
 }
